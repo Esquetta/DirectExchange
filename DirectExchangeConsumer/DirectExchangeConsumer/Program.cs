3e2225d39fb0fabdde28718c24826a69eb9c8c5d
Esquetta <esquetta@gmail.com>
2023-07-09 15:21:31 +0300

    Add project files.

---
+++ DirectExchangeConsumer/DirectExchangeConsumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory connectionFactory = new ConnectionFactory();
+connectionFactory.Uri = new Uri("amqps://nmlgtsid:Hl-ZajX0FHO3eG52BQnrPKc8aQpkYTsd@sparrow.rmq.cloudamqp.com/nmlgtsid");
+
+
+using (IConnection connection = connectionFactory.CreateConnection())
+using (IModel channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare("directexchange", ExchangeType.Direct);
+
+    string queueName = channel.QueueDeclare().QueueName;
+
+    if (int.Parse(args[0])==1)
+        channel.QueueBind(queueName, "directexchange", "teksayi");
+    else
+        channel.QueueBind(queueName, "directexchange", "ciftSayi");
+
+    channel.BasicQos(0, 1, false);
+
+    EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+    channel.BasicConsume(queueName, false, consumer);
+
+    consumer.Received += (sender, e) =>
+    {
+        
+        Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+        channel.BasicAck(e.DeliveryTag, false);
+
+    };
+    Console.Read();
+}
---
+++ DirectExchangePublisher/DirectExchangePublisher/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//Direct Exchange, mesajı direkt olarak ilgili adrese anahtar teslim yapmaktadır diyebiliriz
+
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory connectionFactory = new ConnectionFactory();
+connectionFactory.Uri = new Uri("amqps://nmlgtsid:Hl-ZajX0FHO3eG52BQnrPKc8aQpkYTsd@sparrow.rmq.cloudamqp.com/nmlgtsid");
+
+
+using(IConnection connection=connectionFactory.CreateConnection())
+using(IModel channel=connection.CreateModel())
+{
+    channel.ExchangeDeclare("directexchange", ExchangeType.Direct);
+	for (int i = 0; i < 100; i++)
+	{
+		byte[] byteMessage = Encoding.UTF8.GetBytes($"sayı - {i}");
+
+		IBasicProperties basicProperties = channel.CreateBasicProperties();
+		basicProperties.Persistent = true;
+
+		if (i % 2 == 0)
+			channel.BasicPublish("directexchange", "ciftSayi", basicProperties, byteMessage);
+		else
+            channel.BasicPublish("directexchange", "teksayi", basicProperties, byteMessage);
+
+    }
+}
